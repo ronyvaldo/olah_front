47c356f06e221e39c57f0f9453b986e43b64c77f
ronyvaldo <ronyvaldorbs@gmail.com>
2022-01-04 21:38:29 -0300

    Preparando para deploy...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  apiUrlBase: "https://olah-app.herokuapp.com"
+  apiUrlBase: "https://olah-app.herokuapp.com",
+  clientId: 'olah-app',
+  clientSecret: '-Ol@hS3cr3t#m3mory$&=',
+  tokenUrl: '/oauth/token',
+  secretSocial: 'NXy%uZw7#riU9l0@&'
 };
