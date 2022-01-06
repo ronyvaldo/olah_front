1281b150a5d0ccb2dabdaa4015448128fe3e641b
ronyvaldo <ronyvaldorbs@gmail.com>
2022-01-06 00:53:09 -0300

    Correção para deploy

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname));
 
-/*app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname + '/src/index.html')),
-);*/
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
