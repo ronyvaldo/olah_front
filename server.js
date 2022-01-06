075bc51fea774dd6c89f7ac591dd6a4e48a3f912
ronyvaldo <ronyvaldorbs@gmail.com>
2022-01-06 19:49:16 -0300

    Ajuste para deploy

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname));
+
+console.log("==================");
+
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+
+  console.log("==================");
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/index.html'))
