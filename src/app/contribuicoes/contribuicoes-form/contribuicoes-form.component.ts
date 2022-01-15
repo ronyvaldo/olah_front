1684b29b56954af404d9320e2cf661313f198948
ronyvaldo <ronyvaldorbs@gmail.com>
2022-01-15 19:28:42 -0300

    Ajustando edição de lançamentos

--- src/app/contribuicoes/contribuicoes-form/contribuicoes-form.component.ts
+++ src/app/contribuicoes/contribuicoes-form/contribuicoes-form.component.ts
@@ -106,7 +106,7 @@ export class ContribuicoesFormComponent implements OnInit {
                   this.editando = true;
                   setTimeout(() => {
                     (document.getElementById('idTiposContribuicao') as HTMLInputElement).value = this.contribuicao.tipoContribuicao.id.toString();
-                  }, 200);
+                  }, 250);
                 },
                 errorResponse => this.contribuicao = new Contribuicao()
               )
